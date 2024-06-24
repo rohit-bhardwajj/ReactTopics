819160c82d00ddc35ef686d8c23428539211acf2
Rohit kumar <rohitkumar01498@gmail.com>
2024-06-24 18:49:14 +0530

    Introduction to Context Api

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import UserContext from './Context/UserContext'
+import UserContextProvider from './Context/UserContextProvider'
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+ 
+  return (
+    <UserContextProvider>
+    <h1>Context Api Introduction</h1>
+    <Login/>
+    <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
